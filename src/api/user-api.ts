39d2e7e377d3701c077ebf786d85518ba4bab096
Kira <kors9442@gmail.com>
2022-01-19 11:01:29 +0300

    Update user-api.ts

--- src/api/user-api.ts
+++ src/api/user-api.ts
@@ -1,18 +1,13 @@
+import FetchRequest from '../utils/FetchRequest'
+
 const host = 'https://ya-praktikum.tech/api/v2/user'
+
+const userAPIInstance = new FetchRequest(`${host}`)
 
 export default class UserAPI {
   updatePassword(oldPassword, newPassword) {
-    return fetch(host + '/password', {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      credentials: 'include',
-      body: JSON.stringify({
-        oldPassword,
-        newPassword,
-      }),
-    })
+    return userAPIInstance
+      .put('/password', {data: {oldPassword, newPassword}})
       .then((response) => {
         return response.json()
       })
