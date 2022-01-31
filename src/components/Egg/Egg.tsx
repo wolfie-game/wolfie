c6338a2b8be76fef2650cd5faba2272c1fa7a18c
Alexandra <alexandrapigulevskaya@gmail.com>
2022-02-01 01:23:11 +0300

    Egg edition

--- src/components/Egg/Egg.tsx
+++ src/components/Egg/Egg.tsx
@@ -8,9 +8,21 @@ export default class Egg {
     this.context = ctx
   }
   run() {
+    // egg top
+    this.context.save()
+    this.context.fillStyle = '#ffcc99'
+    this.context.scale(1, 1.75);
+    this.context.beginPath()
+    this.context.arc(75, 43, 20, 0, Math.PI, true);
+    this.context.fill()
+    this.context.restore()
+    // egg bottom
+    this.context.save()
     this.context.fillStyle = '#ffcc99'
     this.context.beginPath()
-    this.context.arc(75, 75, 20, 0, Math.PI * 2, true)
+    this.context.arc(75, 75, 20, 0, Math.PI)
     this.context.fill()
+    this.context.restore()
+
   }
 }
