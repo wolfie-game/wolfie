f1eae57049493b993de7cab71253c4e7f355759b
Alexandra <alexandrapigulevskaya@gmail.com>
2022-03-13 13:38:03 +0300

    update

--- src/Client.tsx
+++ src/Client.tsx
@@ -1,23 +1,10 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom'
-// import './components/App/App.scss'
-
-// const Client = () => {
-//   return (
-//     <h1>hi</h1>
-//   )
-// }
-
-// export default Client
 import * as React from 'react'
 import {hydrate} from 'react-dom'
 import {BrowserRouter} from 'react-router-dom'
 import {Provider as ReduxProvider} from 'react-redux'
-import {loadableReady} from '@loadable/component'
-// import 'babel-polyfill'
-
+// import {loadableReady} from '@loadable/component'
 import App from './components/App/App'
-// import { State } from 'types'
+// import {State} from 'types'
 import {configureStore} from './utils/redux/store'
 
 const {store, history} = configureStore(window.__INITIAL_STATE__);
@@ -30,13 +17,12 @@ declare global {
     }
 }
 
-loadableReady(() => {
-  hydrate(
+hydrate(
     <Provider store={store}>
         <BrowserRouter history={history}>
             <App />
         </BrowserRouter>
     </Provider>,
     document.getElementById('wolfie'),
-  )
-})
+)
+
