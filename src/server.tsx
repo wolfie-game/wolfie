f647bf461e100b60d64c4cf250b9bd01d787c630
Alexandra <alexandrapigulevskaya@gmail.com>
2022-03-13 15:59:56 +0300

    Service worker fix

--- src/server.tsx
+++ src/server.tsx
@@ -9,6 +9,9 @@ const app = express()
 app.use(express.static(path.resolve(__dirname, '../build')))
     .use(express.static(path.resolve(__dirname, '../static')))
 
+app.get("/sw.js", (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../public', 'sw.js'));
+});
 app.get('/*', serverRenderMiddleware)
 
 export {app}
