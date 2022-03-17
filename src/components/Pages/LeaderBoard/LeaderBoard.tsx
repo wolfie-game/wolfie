77cc432b48bbeff042cfb7ff0ec064ef8eaf41fa
Pigulevskayas <alexandrapigulevskaya@gmail.com>
2022-03-17 17:37:58 +0300

    Leaderboard if empty array

--- src/components/Pages/LeaderBoard/LeaderBoard.tsx
+++ src/components/Pages/LeaderBoard/LeaderBoard.tsx
@@ -17,6 +17,28 @@ function LeaderBoard(props) {
     // console.log('checkin store', props)
   }, [])
 
+  if(!props.user.auth) {
+    return(
+      <ErrorBoundary>
+        <PageMeta title="Лидерборд" description="Рейтинг игроков"/>
+        <div className="content__canvas">
+          <div className="simpple-page">
+            <div className="simpple-page__heading">
+              <h1 className="simpple-page__title">Leaderboard</h1>
+              <Button
+                styleName="button-back"
+                type="button"
+                handler={() => navigate(-1)}></Button>
+            </div>
+            <div className="leaderboard">
+              <h2 className="error-title">Авторизуйтесть</h2>
+            </div>
+          </div>
+        </div>
+      </ErrorBoundary>
+    );
+  }
+
   return (
     <ErrorBoundary>
       <PageMeta title="Лидерборд" description="Рейтинг игроков"/>
@@ -39,7 +61,7 @@ function LeaderBoard(props) {
                   />
               ))
             ) : (
-              <h2 className="error-title">Авторизуйтесть</h2>
+              <p>...</p>
             )}
           </div>
         </div>
