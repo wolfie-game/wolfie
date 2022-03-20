f4526efabd88c4023ecd3ae48997fb90e6aa5ad5
Kira <kors9442@gmail.com>
2022-03-20 22:36:22 +0300

    Update topics-api.ts

--- src/api/topics-api.ts
+++ src/api/topics-api.ts
@@ -7,14 +7,14 @@ import {
 
 export default class TopicsAPI {
   getAllTopics() {
-  /*  return sequelize.query(`
+   return sequelize.query(`
     SELECT t.ID, t.TITLE, t.CONTENT, u.LOGIN, u.AVATAR, COUNT(m.ID) AS COMMENTS_COUNT
     FROM RPS_TOPIC t
           LEFT JOIN RPS_USER u ON u.ID = t.AUTHOR_ID
           LEFT JOIN RPS_COMMENT m ON m.TOPIC_ID = t.ID
     GROUP BY t.ID, t.TITLE, t.CONTENT, u.LOGIN, u.AVATAR
     ORDER BY t.created_at
-  `)*/
+  `)
   }
 
   getTopic(id) {
