84abfbf493c0933c7c3d70627657713c15ba761c
Pigulevskayas <alexandrapigulevskaya@gmail.com>
2022-03-22 02:19:07 +0300

    User router

---
+++ src/server-db/router/themesRouter.ts
@@ -0,0 +1,12 @@
+import {Router} from 'express'
+import {ThemeAPI} from '../api/theme-api'
+
+export const themesRoutes = (router: Router) => {
+  const themesRouter: Router = Router()
+
+  themesRouter
+    .post('/', ThemeAPI.update)
+    .get('/', ThemeAPI.get)
+
+  router.use('/theme', themesRouter)
+}
--- src/server.tsx
+++ src/server.tsx
@@ -4,6 +4,7 @@ import compression from 'compression'
 import 'babel-polyfill'
 import serverRenderMiddleware from './server-render-middleware'
 import {dbConnect} from './server-db/sequelize'
+import router from './server-db/router/router'
 
 const app = express()
 
@@ -12,6 +13,8 @@ dbConnect()
 app
   .use(express.static(path.resolve(__dirname, '../build')))
   .use(express.static(path.resolve(__dirname, '../static')))
+  .use(router)
+
 
 app.get('/sw.js', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../public', 'sw.js'))
