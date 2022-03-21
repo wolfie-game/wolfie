e13d2a166357495302b406cb5e27653a1a6da80a
Kira <kors9442@gmail.com>
2022-03-21 21:53:29 +0300

    topics

--- src/api/topics-api.ts
+++ src/api/topics-api.ts
@@ -1,50 +1,47 @@
-import {sequelize} from '../server-db/sequelize'
-import {
-  Topic,
-  TopicCreateAttributes,
-  TopicUpdateAttributes,
-} from '../server-db/models/topic'
+import {topicService} from 'server-db/services/TopicService'
 
-export default class TopicsAPI {
-  getAllTopics() {
-   return sequelize.query(`
-    SELECT t.ID, t.TITLE, t.CONTENT, u.LOGIN, u.AVATAR, COUNT(m.ID) AS COMMENTS_COUNT
-    FROM RPS_TOPIC t
-          LEFT JOIN RPS_USER u ON u.ID = t.AUTHOR_ID
-          LEFT JOIN RPS_COMMENT m ON m.TOPIC_ID = t.ID
-    GROUP BY t.ID, t.TITLE, t.CONTENT, u.LOGIN, u.AVATAR
-    ORDER BY t.created_at
-  `)
-  }
+export class TopicAPI {
+  public static create = async (req, res) => {
+    const {body} = req
 
-  getTopic(id) {
-    return sequelize.query(
-      `
-    SELECT t.ID, t.TITLE, t.CONTENT, u.LOGIN, u.AVATAR, COUNT(m.ID) AS COMMENTS_COUNT
-    FROM RPS_TOPIC t
-          LEFT JOIN RPS_USER u ON u.ID = t.AUTHOR_ID
-          LEFT JOIN RPS_COMMENT m ON m.TOPIC_ID = t.ID
-    WHERE t.ID = ?
-    GROUP BY t.ID, t.TITLE, t.CONTENT, u.LOGIN, u.AVATAR
-    ORDER BY t.created_at
-  `,
-      {replacements: [id]},
-    )
-  }
-  find = (id: number) => {
-    return Topic.findOne({
-      where: {
-        id: id,
-      },
-    })
-  }
-  async postTopic(data) {
-    const record = await this.find(data.id)
-    if (record === null) {
-      throw new Error('Topic not found')
+    try {
+      const topic = await topicService.create(body)
+      res.json({message: 'Topic created', topic})
+    } catch (e) {
+      res.status(400)
+      res.json({error: e.message})
     }
-    record.set(data)
+  }
 
-    return record.save()
+  public static update = async (req, res) => {
+    const {body} = req
+
+    try {
+      await topicService.update(body)
+      res.json({message: 'Topic updated'})
+    } catch (e) {
+      res.status(400)
+      res.json({error: e.message})
+    }
+  }
+
+  public static get = async (req, res) => {
+    const {query} = req
+    const {id} = query
+    let response
+    try {
+      if (id) {
+        const [results] = await topicService.request(Number(id))
+        response = results[0]
+      } else {
+        const [results] = await topicService.findAll()
+        response = results
+      }
+
+      res.json(response)
+    } catch (e) {
+      res.status(404)
+      res.json({error: e.message})
+    }
   }
 }
