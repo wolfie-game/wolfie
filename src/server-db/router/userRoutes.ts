952385e18946222cf28a24b9528cf0f1c44e96f0
Kira <kors9442@gmail.com>
2022-03-22 19:46:41 +0300

    Create userRoutes.ts

---
+++ src/server-db/router/userRoutes.ts
@@ -0,0 +1,13 @@
+import {Router} from 'express'
+import {UserAPI} from '../../api/user-api-db'
+
+export const themesRoutes = (router: Router) => {
+  const themesRouter: Router = Router()
+
+  themesRouter
+  .get('/', UserAPI.get)
+  .post('/create', UserAPI.update)
+  .post('/update', UserAPI.update)
+
+  router.use('/user', themesRouter)
+}
